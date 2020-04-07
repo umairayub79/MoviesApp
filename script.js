b6674b099d03e44cec0aed4099667bad1ca49d4d
umairayub79 <umairayub79@gmail.com>
2020-04-08 00:20:46 +0500

    implemented Pagination.js for Pagination

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 const moviesContainer = document.getElementById("movies-container");
-const URL = "https://yts.mx/api/v2/list_movies.json?limit=50";
+const URL = "https://yts.mx/api/v2/list_movies.json";
 let page = 1;
 const observer = window.lozad();
 observer.observe();
 
-function getMovies(url) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      for (let index = 0; index < 50; index++) {
-        const item = {
-          id: data.data.movies[index].id,
-          title: data.data.movies[index].title,
-          url: data.data.movies[index].url,
-          rating: data.data.movies[index].rating,
-          year: data.data.movies[index].year,
-          cover: data.data.movies[index].medium_cover_image,
-          links: data.data.movies[index].torrents
-        };
-        createMovieView(item)
-      }
-      
-    });
-    if(page < 3){
-      getMovies(url + `&page=${page += 1}`);
-    }
+$("#container").pagination({
+  alias: {
+    pageNumber: "page",
+    pageSize: "limit",
+  },
+  dataSource: URL,
+  locator: "data.movies",
+  pageSize: 20,
+  totalNumber: 16140,
+  pageNumber:1,
+  ajax: {
+    beforeSend: function () {
+      $("#movies-container").html("<h1>Loading Movies.....</h1>");
+    },
+  },
+  callback: function (data, pagination) {
+    $("#movies-container").html("")
+    getMovies(data);
+  },
+});
+
+function getMovies(items) {
+  for (let index = 0; items.length; index++) {
+    const item = {
+      id: items[index].id,
+      title: items[index].title,
+      url: items[index].url,
+      rating: items[index].rating,
+      year: items[index].year,
+      cover: items[index].medium_cover_image,
+      links: items[index].torrents,
+    };
+    createMovieView(item);
+  }
 }
-
-getMovies(URL + `&page=${page}`);
-
-
 function createMovieView(movie) {
   const movieView = document.createElement("div");
   movieView.classList.add("movie");
@@ -39,7 +48,6 @@ function createMovieView(movie) {
   movieCover.classList.add("movieCover");
   movieCover.classList.add("lozad");
   movieCover.src = movie.cover;
-
 
   const movieTitle = document.createElement("h3");
   movieTitle.classList.add("movieTitle");
@@ -56,7 +64,6 @@ function createMovieView(movie) {
   movieRating.classList.add("movieRating");
   movieRating.innerText = `Rating ${movie.rating} `;
 
-  
   movieInfo.appendChild(movieYear);
   movieInfo.appendChild(movieRating);
 
@@ -64,13 +71,12 @@ function createMovieView(movie) {
   movieView.appendChild(movieTitle);
   movieView.appendChild(movieInfo);
 
-for (let index = 0; index < movie.links.length; index++) {
-      const downloadButton = document.createElement('a');
-      downloadButton.href = movie.links[index].url;
-      downloadButton.innerText = `Torrent ${movie.links[index].quality}`;
+  for (let index = 0; index < movie.links.length; index++) {
+    const downloadButton = document.createElement("a");
+    downloadButton.href = movie.links[index].url;
+    downloadButton.innerText = `Torrent ${movie.links[index].quality}`;
 
-      movieView.appendChild(downloadButton);
-      
+    movieView.appendChild(downloadButton);
   }
   moviesContainer.appendChild(movieView);
 }
